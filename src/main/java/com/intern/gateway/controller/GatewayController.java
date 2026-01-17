2c5b175cd65d10674afd24975f0c4d444051c2da
Ioann Yaromenka <ioann.yaromenka@innowise.com>
2026-01-17 16:51:34 +0300

    OpenApi controller descriptions

--- src/main/java/com/intern/gateway/controller/GatewayController.java
+++ src/main/java/com/intern/gateway/controller/GatewayController.java
@@ -5,6 +5,8 @@ import com.intern.gateway.dto.UserLoginRequest;
 import com.intern.gateway.dto.UserRegistrationRequest;
 import com.intern.gateway.dto.UserResponse;
 import com.intern.gateway.service.GatewayService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,7 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/")
+@Tag(name = "Gateway", description = "Gateway API for user authentication and system registration.")
 public class GatewayController {
 
     private final GatewayService gatewayService;
@@ -25,12 +28,14 @@ public class GatewayController {
     }
 
     @PostMapping("/register")
+    @Operation(summary = "Register user", description = "Registers a new user profile across the identity provider (Keycloak) and the internal User Service.")
     public Mono<ResponseEntity<UserResponse>> register(@RequestBody UserRegistrationRequest request) {
         return gatewayService.registerUser(request)
                 .map(response -> ResponseEntity.ok().body(response));
     }
 
     @PostMapping("/login")
+    @Operation(summary = "User login", description = "Authenticates user credentials and returns a valid JWT access token.")
     public Mono<ResponseEntity<LoginResponse>> login(@RequestBody UserLoginRequest request) {
         return gatewayService.loginUser(request)
                 .map(token ->  ResponseEntity.ok().body(token));
